9accc8115fc8daf546e77c575c998a308acfbc88
Ed Summers <ehs@pobox.com>
2025-05-27 17:57:28 -0400

    remove unused things

--- src/podbucket/convert.py
+++ src/podbucket/convert.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from podbucket.resourcesync import get_resources, get
 
 
 def convert_marcxml(marcxml_url: str, name: str, storage_root: Path):
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from podbucket.convert import convert_marcxml
 
 
@@ -6,6 +5,6 @@ def test_convert(tmp_path):
     marcxml_url = (
         "https://pod.stanford.edu/file/473765/stanford-2025-05-20-delta-marcxml.xml.gz"
     )
-    parquet_path = convert_marcxml(marcxml_url, name="stanford", storage_root=tmp_path)
+    convert_marcxml(marcxml_url, name="stanford", storage_root=tmp_path)
     # assert parquet_path
     # assert parquet_path.is_file()
